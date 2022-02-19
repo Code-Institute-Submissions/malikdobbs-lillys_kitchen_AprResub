659c6857df8d3ed3d88f345b14f01a216f3d419f
malikdobbs <malik-dobbs@hotmail.com>
2022-02-19 09:50:26 +0000

    Search functionality added to recipes page and tidied up edit and delete buttons on recipes page

--- app.py
+++ app.py
@@ -36,6 +36,16 @@ def get_recipes():
     return render_template("get_recipes.html", recipes=recipes)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    """
+    Placeholder
+    """
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("get_recipes.html", recipes=recipes)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """
